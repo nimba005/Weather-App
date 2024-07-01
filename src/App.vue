6b52582d8025744a99bbf1f18256e1f3e0e8d8f4
nimba005 <nimbaoyugi@gmail.com>
2024-07-01 16:48:39 +0300

    update

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <weatherApp />
+  </div>
+</template>
+
+<script>
+import weatherApp from './components/weatherApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    weatherApp
+  }
+}
+</script>
+
+<style>
+</style>
